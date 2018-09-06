45d4313c8e43c82a69fe4b958fa21366ac0535d5
ManuelWe <manuel.wetzel@hpe.com>
2018-09-06 19:00:02 +0200

    Added Angular and ESLint

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+/* eslint new-cap: [2, {capIsNewExceptions: ["Router"]}]*/
+const express = require('express');
+const router = express.Router();
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+module.exports = router;
