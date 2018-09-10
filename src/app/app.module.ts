8a82070d282e57fd5d4fbf2b00fb5670b6414bba
Ovakefali13 <Ovakefali13@web.de>
2018-09-10 10:51:15 +0200

    Footer component works now
    Implemented angular-bootstrap-mdb (NOT WORKING)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent, FooterComponent, NavbarComponent
+  ]
 })
 export class AppModule { }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   name = 'footer';
+  tag;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
