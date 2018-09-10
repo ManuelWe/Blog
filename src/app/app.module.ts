6efacc1808112cc40f440e06ca7fd74561483dc3
Ovakefali13 <Ovakefali13@web.de>
2018-09-10 16:22:40 +0200

    Basic Navbar, Footer and Body implemented
    Not finished, only basic structure and css tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, NavbarComponent, FooterComponent],
   schemas: [ NO_ERRORS_SCHEMA ],
 })
 export class AppModule { }
