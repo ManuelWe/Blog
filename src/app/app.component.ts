994d22370e9f45d6f759a38fb80d1cb202c2ac3a
Ovakefali13 <Ovakefali13@web.de>
2018-09-17 17:03:28 +0200

    Additions in login/register - Not working
    Issue:
    - can't get text from inputfield
    - authentication with database fails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,28 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class AppComponent {
   allArticles;
+  allUsers;
   randomArticle = '';
-  loggedIn: boolean;
 
-  loginPassword = new FormControl('', Validators.email);
-  loginEmail = new FormControl('', Validators.required);
+  loggedIn: boolean;
+  loginPasswordFC = new FormControl('', Validators.required);
+  loginEmailFC = new FormControl('', Validators.email);
+  loginEmail = 'bernd@blog.com';
+  loginPassword = 'Pa$$w0rd';
+  loginUserId;
+
+  registerObject;
+  registerFirstname;
+  registerLastname;
+  registerEmail;
+  registerCity;
+  registerZipcode;
+  registerStreet;
+  registerStreetnumber;
+  registerPicture;
+  registerPassword;
+
+
   signupFormModalName = new FormControl('', Validators.required);
   signupFormModalEmail = new FormControl('', Validators.email);
   signupFormModalPassword = new FormControl('', Validators.required);
@@ -22,16 +39,48 @@ export class AppComponent {
     this.myFirstService.getAllArticles().subscribe(data => {
       this.allArticles = data;
     });
+    this.myFirstService.getAllUsers().subscribe(data => {
+      this.allUsers = data;
+    });
   }
   setRandomArticle() {
     this.randomArticle = this.allArticles[Math.floor(Math.random() * (this.allArticles.length + 1))]._id;
   }
   login() {
-
+    for (let user of this.allUsers) {
+      console.log(user.email);
+      console.log(this.loginEmail);
+      if (user.email === this.loginEmail) {
+        this.loginUserId = user._id;
+      }
+    }
+    this.myFirstService.login(this.loginUserId, this.loginPassword);
     this.loggedIn = true;
   }
   logout() {
+    /*this.loginEmail = '';
+    this.loginPassword = '';
+    this.loginUserId = '';*/
     this.loggedIn = false;
+  }
+  register() {
+    for (let user of this.allUsers) {
+      if (user.email === this.loginEmail) {
+        return; // do a e-mail validation here
+      }
+    }
+    this.registerObject = {
+      "zipCode": this.registerZipcode,
+      "firstname": this.registerFirstname,
+      "password": this.registerPassword,
+      "city": this.registerCity,
+      "streetNumber": this.registerStreetnumber,
+      "street": this.registerStreet,
+      "email": this.registerEmail,
+      "picture": this.registerPicture,
+      "lastname": this.registerLastname
+    }
+    this.myFirstService.register(this.registerObject);
   }
 }
 
--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -9,6 +9,9 @@ export class RecordsService {
   getAllArticles() {
     return this.http.get('http://localhost:3000/api/articles');
   }
+  getAllUsers() {
+    return this.http.get('http://localhost:3000/api/users');
+  }
   getArticle(articleId) {
     return this.http.get('http://localhost:3000/api/articles/' + articleId);
   }
@@ -19,6 +22,14 @@ export class RecordsService {
     return this.http.get('http://localhost:3000/api/users/' + userId);
   }
   login(userId, password) {
-    console.log(this.http.post('http://localhost:3000/api/users/' + userId + '/authenticate', '{ password: ' + password + '}'));
+    console.log(userId);
+    console.log(password);
+    const loginObject = {
+      'password': password
+    };
+    console.log(this.http.post('http://localhost:3000/api/users/' + userId + '/authenticate', loginObject));
+  }
+  register(registerObject) {
+    console.log(this.http.post('http://localhost:3000/api/users/', <JSON> registerObject));
   }
 }
