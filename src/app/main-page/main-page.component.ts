d2607717210d9c1b51338578830ddc283d991688
Ovakefali13 <Ovakefali13@web.de>
2018-09-28 12:42:24 +0200

    Pictures on main-page fixed

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -11,12 +11,14 @@ export class MainPageComponent implements OnInit {
   allArticles;
   articles = [];
   authors = [];
+  pictures = [];
   constructor(private route: ActivatedRoute, private myFirstService: RecordsService) {
     this.myFirstService.getAllArticles().subscribe(data => {
       this.allArticles = data;
       this.articles.push(this.allArticles[0]);
       this.articles.push(this.allArticles[1]);
       this.articles.push(this.allArticles[2]);
+      console.log(this.articles[0]._id);
       this.myFirstService.getUser(this.articles[0].author).subscribe(data1 => {
         this.authors.push(data1);
       });
@@ -25,6 +27,15 @@ export class MainPageComponent implements OnInit {
       });
       this.myFirstService.getUser(this.articles[2].author).subscribe(data1 => {
         this.authors.push(data1);
+      });
+      this.myFirstService.getArticle(this.articles[0]._id).subscribe(data1 => {
+        this.pictures.push(data1.picture);
+      });
+      this.myFirstService.getArticle(this.articles[1]._id).subscribe(data1 => {
+        this.pictures.push(data1.picture);
+      });
+      this.myFirstService.getArticle(this.articles[2]._id).subscribe(data1 => {
+        this.pictures.push(data1.picture);
       });
     });
   }
