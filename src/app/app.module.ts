c68dad6504688dd1d1e3dcc70c626929f8488f16
ManuelWe <manuel.wetzel@hpe.com>
2018-10-08 11:12:03 +0200

    Merge error fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {ErrorHandler, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {HttpClientModule} from '@angular/common/http';
 import {RecordsService} from './records.service';
@@ -36,6 +36,14 @@ const routes: Routes = [
   {
     path: 'createArticle',
     component: CreateArticleComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
   }
 ];
 
@@ -47,7 +55,8 @@ const routes: Routes = [
     AllarticlesComponent,
     CommentsComponent,
     CreateArticleComponent,
-    CreateCommentComponent
+    CreateCommentComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +65,12 @@ const routes: Routes = [
     MDBBootstrapModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
-  providers: [RecordsService],
+  providers: [RecordsService,
+    AppComponent,
+    {
+      provide: ErrorHandler,
+      useClass: ErrorsHandler
+    }],
   bootstrap: [AppComponent],
   schemas: [ NO_ERRORS_SCHEMA ],
 })
