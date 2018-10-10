5b8f617381cef896d23d0a76283e745c1303bbe1
ManuelWe <manuel.wetzel@hpe.com>
2018-10-10 23:42:38 +0200

    All comments from article fixed; gulp validation added

---
+++ SwaggerBackend/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    rules: {
+      "max-len": ["error", {"code": 110}],
+	  "linebreak-style": ["error", "windows"]
+    }
+};
